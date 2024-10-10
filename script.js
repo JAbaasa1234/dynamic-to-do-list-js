835b84622849c6d8004772d7dbf460ecb83d38f6
Jerry Abaasa <jerryabaasa@gmail.com>
2024-10-11 00:55:21 +0300

    Implemented Local Storage to To-Do List Application.

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Create a remove button for the task
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
 
         // Attach an event to the remove button to delete the task when clicked
         removeButton.onclick = function() {
